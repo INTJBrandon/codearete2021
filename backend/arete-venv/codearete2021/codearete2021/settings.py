38ac8a00dba32e7782f580331c0f97408748cd50
seireitou <ausis@mail.com>
2021-10-06 23:04:16 -0500

    creates app for api and installs the app into the settings.py

--- backend/arete-venv/codearete2021/codearete2021/settings.py
+++ backend/arete-venv/codearete2021/codearete2021/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projectapi.apps.ProjectapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
