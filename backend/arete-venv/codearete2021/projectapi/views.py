5bc3342b6deb83819e0b65dad5a257b82936cdea
seireitou <ausis@mail.com>
2021-10-07 12:40:17 -0500

    sets up the viewset for django rest framework for project objects

--- backend/arete-venv/codearete2021/projectapi/views.py
+++ backend/arete-venv/codearete2021/projectapi/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProjectSerializer
+from .models import Project
 
 # Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all().order_by('title')
+    serializer_class = ProjectSerializer
