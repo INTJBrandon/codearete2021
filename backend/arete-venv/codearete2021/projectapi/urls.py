b983c20738eb4caa61871862714d716e5a58fb99
seireitou <ausis@mail.com>
2021-10-07 19:31:19 -0500

    sets up url patterns to include viewsets for projects

--- backend/arete-venv/codearete2021/projectapi/urls.py
+++ backend/arete-venv/codearete2021/projectapi/urls.py
@@ -1,8 +1,15 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework import routers, urlpatterns
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r'projects', views.ProjectViewSet)
 
 # Wire up our API using automatic URL Routing 
+# Additionally, we include login URLs for the browsable API.
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls',
+    namespace='rest_framework'))
+]
