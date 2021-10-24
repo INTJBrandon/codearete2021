d3040d294bf67d67051ef878bb73dd194d8a7e23
seireitou <ausis@mail.com>
2021-10-23 22:40:58 -0500

    changes link names for navbar

--- frontend/codearete/src/navbar.js
+++ frontend/codearete/src/navbar.js
@@ -8,8 +8,8 @@ function Mainnavbar() {
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link href="#features">Portfolio</Nav.Link>
+                <Nav.Link href="#pricing">Resume</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
